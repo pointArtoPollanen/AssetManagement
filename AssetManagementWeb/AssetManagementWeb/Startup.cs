c5993b61ea8b36fd10ac7b96120948b2df0d93e1
Arto Pöllänen <arto.pollanen@stud.pointcollege.fi>
2016-04-05 13:30:16 +0300

    ASP.NET MVC -projektin luonti

---
+++ AssetManagementWeb/AssetManagementWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AssetManagementWeb.Startup))]
+namespace AssetManagementWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
